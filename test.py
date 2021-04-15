58a2882acc2967eaa98c674edf887722f1dc2740
Matthias Roels <matthias.roels21@gmail.com>
2021-04-15 11:45:07 +0200

    Extended test script to test logging an artifact

--- test.py
+++ test.py
@@ -1,21 +1,40 @@
 """Little script to test if the tracking server works"""
+import os
 import mlflow
 
-mlflow.set_tracking_uri("http://localhost:32172")
 
-mlflow.set_experiment("mlflow-testing")
+# get port from running kubectl get services
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:30760/"
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
 
-mlflow.start_run()
 
-mlflow.log_params(
-    {
-        "n_estimators": 300,
-        "max_depth": 6,
-        "learning_rate": 0.03,
-        "min_child_weight": 50,
-        "gamma": 0.001,
-        "reg_lambda": 500,
-    }
-)
+def main():
 
-mlflow.end_run()
+    features = "rooms, zipcode, median_price, school_rating, transport"
+    with open("features.txt", 'w') as f:
+        f.write(features)
+
+
+    mlflow.set_tracking_uri("http://localhost:30496")
+
+    mlflow.set_experiment("mlflow-testing")
+
+    with mlflow.start_run():
+
+        mlflow.log_params(
+            {
+                "n_estimators": 300,
+                "max_depth": 6,
+                "learning_rate": 0.03,
+                "min_child_weight": 50,
+                "gamma": 0.001,
+                "reg_lambda": 500,
+            }
+        )
+
+        mlflow.log_artifact("features.txt")
+
+
+if __name__ == '__main__':
+    main()
